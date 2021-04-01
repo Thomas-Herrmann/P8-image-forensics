a81c418fe2b258b491779d182e98d6a4f35e0981
Thomas Herrmann <48517973+Thomas-Herrmann@users.noreply.github.com>
2021-04-01 22:00:25 +0200

    Implemented a class of which objects enable generation of pairs of partially manipulated images and corresponding masks. Does not support baching or rotation of masks yet.

---
+++ DataGenerator.py
@@ -0,0 +1,78 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import cv2 as cv
+import ntpath
+
+
+class DataGenerator():
+
+    def __init__(self, mask_dir_path, man_dir_path, crop_shape=(256, 256)):
+        
+        self.man_dir_path = man_dir_path
+
+        self.masks = tfds.folder_dataset.ImageFolder(mask_dir_path)              \
+                                        .as_dataset(shuffle_files=True)["masks"] \
+                                        .repeat()                                \
+                                        .as_numpy_iterator()
+        
+        self.mans = tfds.folder_dataset.ImageFolder(man_dir_path)                     \
+                                       .as_dataset(shuffle_files=True)["manipulated"] \
+                                       .repeat()                                      \
+                                       .as_numpy_iterator()
+        
+        self.crop_h, self.crop_w = crop_shape
+
+
+    def _make_mask(self):
+
+        mask     = self.masks.next()["image"]
+        cropped  = tf.image.random_crop(mask, (self.crop_h, self.crop_w, 3))
+        reshaped = tf.reshape(cropped, (1, self.crop_h, self.crop_w, 3))
+        dilated  = tf.nn.dilation2d(reshaped, tf.zeros((1, 1, 3), tf.uint8), (1, 1, 1, 1), "SAME", "NHWC", (1, 1, 1, 1))
+        
+        return tf.reshape(dilated, (self.crop_h, self.crop_w, 3))
+
+
+    def _get_cropped_img_pair(self):
+
+        man_dic          = self.mans.next()
+        width, height, _ = man_dic["image"].shape
+
+        rand_gen     = tf.random.get_global_generator()
+        h_crop_start = rand_gen.uniform([], 0, height - self.crop_h, tf.int32)
+        w_crop_start = rand_gen.uniform([], 0, width - self.crop_w, tf.int32)
+        
+        cropf = lambda img: img[w_crop_start:w_crop_start + self.crop_w, h_crop_start:h_crop_start + self.crop_h]
+
+        man_crop = cropf(man_dic["image"])
+        basename = ntpath.basename(man_dic["image/filename"].decode("utf-8"))
+        pri_path = ntpath.splitext(ntpath.join(ntpath.join(self.man_dir_path, "pristine"), basename))[0] + ".jpg"
+        pri_crop = cropf(cv.cvtColor(cv.imread(pri_path), cv.COLOR_BGR2RGB))
+
+        return pri_crop, man_crop
+
+
+    def _join_by_mask(self, one, two, mask):
+
+        mapped_neg_mask = tf.cast(mask / 255, tf.int32)
+        mapped_mask     = tf.ones((self.crop_h, self.crop_w, 3), tf.int32) - mapped_neg_mask
+
+        return tf.einsum("ijk, ijk -> ijk", one, mapped_neg_mask) \
+             + tf.einsum("ijk, ijk -> ijk", two, mapped_mask)
+
+
+    def next(self):
+
+        mask                  = self._make_mask()
+        pristine, manipulated = self._get_cropped_img_pair()
+        combined              = self._join_by_mask(pristine, manipulated, mask)
+
+        return combined, mask
+
+
+
+    
+
+
+
+
