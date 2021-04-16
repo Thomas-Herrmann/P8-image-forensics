d4936494db75581734e7bfc55ed83cb064ab359f
seblund <sebastianlund112@gmail.com>
2021-04-16 15:37:00 +0200

    Move qualitive test main into main function

--- qualitative_test.py
+++ qualitative_test.py
@@ -3,8 +3,9 @@ import random
 import cv2 as cv
 import PIL
 from manipulations import ManiFamily
+import numpy as np
 
-
+    
 def colored_mask(mask, seed=13):
 
     make_table = lambda ch_dict: tf.lookup.StaticVocabularyTable(tf.lookup.KeyValueTensorInitializer(
@@ -41,12 +42,27 @@ def make_legends_image(legends):
     return tf.cast(tf.concat([make_col_square(n) for n in range(9)], 0), tf.uint8)
 
 
-model  = tf.keras.models.load_model("test_checkpoint.tf")
-image  = tf.image.random_crop(cv.cvtColor(cv.imread("test_img.png"), cv.COLOR_BGR2RGB), [256, 256, 3])
+def f1(y_true, y_pred):
+    return 1
 
-c_mask, legends = colored_mask(tf.math.argmax(model(tf.expand_dims(image, 0)), axis=-1))
 
-tf.io.write_file("test_out.png", tf.io.encode_png(tf.reshape(c_mask, [256, 256, 3])))
-tf.io.write_file("test_in.png", tf.io.encode_png(image))
-tf.io.write_file("test_legends.png", tf.io.encode_png(make_legends_image(legends)))
+if __name__ == "__main__":
+    model  = tf.keras.models.load_model("pixel_aaconv_save_at_2.tf", custom_objects={'f1':lambda x,y:1})
+    for i in range(10):
+        image  = tf.image.decode_png(tf.io.read_file(f"samples/image{i}.png"), channels=3)
 
+        mask = tf.image.decode_png(tf.io.read_file(f"samples/mask{i}.png"), channels=3)
+
+        #tf.image.random_crop(cv.cvtColor(cv.imread("test_img.png"), cv.COLOR_BGR2RGB), [256, 256, 3])
+        out = model(tf.expand_dims(image, 0))
+        
+        mean_logits = tf.math.reduce_max(out, [0,1,2])
+        print(np.around(tf.nn.softmax(mean_logits).numpy(), 2))
+        #_, top = tf.math.top_k(out, k=8)
+        #print(top)
+        c_mask, legends = colored_mask(tf.math.argmax(out, axis=-1))
+        #c_mask, legends = colored_mask(tf.gather(top, [1], axis=-1))
+
+        tf.io.write_file(f"test_out{i}.png", tf.io.encode_png(tf.concat([image, mask, tf.reshape(c_mask, [256, 256, 3])], axis=1))) 
+        #tf.io.write_file("test_in.png", tf.io.encode_png(image))
+    tf.io.write_file("test_legends.png", tf.io.encode_png(make_legends_image(legends)))
