7f6f2a51f3fd503941dc875ede67d7dcfbef3675
Sebastian Lund <sebastianlund112@gmail.com>
2021-04-27 08:23:54 +0200

    fix

--- DataGenerator.py
+++ DataGenerator.py
@@ -252,9 +252,9 @@ def get_weighted_two_class_dataset(batch_size, weights):
 #for images, masks in get_combined_dataset(2):
 #    print(".")
 if __name__ == "__main__":
-    generate_validation()
+    #generate_validation()
     path = os.path.abspath("validation")
-    tf.data.experimental.save(get_combined_dataset(batch_size=64, split='test').unbatch(), path)
+    tf.data.experimental.save(get_combined_dataset(batch_size=64, split='validation').unbatch(), path)
     '''
     i = 0
     for images, masks in get_combined_dataset(64):
